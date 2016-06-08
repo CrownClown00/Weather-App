194cb7f1a066b50b383278ba7c5f31b7da2edb5a
CrownClown00 <rossca.thompson@gmail.com>
2016-06-08 16:15:04 -0400

    update
    bug fixes

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var success=function(position){
            var coordinates=position.coords;
            var longitude=coordinates.longitude;
            var latittude=coordinates.latitude;
-        var url="https://api.openweathermap.org/data/2.5/forecast/weather?lat="+latittude+"&lon="+longitude+"&APPID=038243b3b5b7501af0c45899e32c9e66";
+        var url="http://api.openweathermap.org/data/2.5/forecast/weather?lat="+latittude+"&lon="+longitude+"&APPID=038243b3b5b7501af0c45899e32c9e66";
             var data;
             var callback=function(response){
                 console.log(response.weather);
@@ -21,7 +21,7 @@ var success=function(position){
       navigator.geolocation.getCurrentPosition(success)
         
 
-console.log(longitude);
+
 
 
 })
