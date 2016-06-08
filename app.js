c22203f33102abcfb39e9f3faf4f0a6f38869e47
CrownClown00 <rossca.thompson@gmail.com>
2016-06-08 16:04:07 -0400

    updates
    updated stylesheet and app js

--- app.js
+++ app.js
@@ -1,20 +1,27 @@
 $(document).ready(function(){
 
+
+
+
 var success=function(position){
            var coordinates=position.coords;
-          
            var longitude=coordinates.longitude;
            var latittude=coordinates.latitude;
-          
-          console.log(latitude);
-          console.log(longitude);
+        var url="http://api.openweathermap.org/data/2.5/forecast/weather?lat="+latittude+"&lon="+longitude+"&APPID=038243b3b5b7501af0c45899e32c9e66";
+            var data;
+            var callback=function(response){
+                console.log(response.weather);
+            };    
 
-          
-      }
+          $.getJSON(url,callback);
+      
+    
+
+}
       navigator.geolocation.getCurrentPosition(success)
         
 
-
+console.log(longitude);
 
 
 })
