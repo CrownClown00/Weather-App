6a97ed1046859eacd4d8e6bf8271239a725595c7
CrownClown00 <rossca.thompson@gmail.com>
2016-06-08 16:36:28 -0400

    update
    bug fixes

--- app.js
+++ app.js
@@ -1,27 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
+  var success = function(position) {
+    var coordinates = position.coords;
+    var longitude = coordinates.longitude;
+    var latittude = coordinates.latitude;
+    var url = "api.openweathermap.org/data/2.5/forecast/weather?lat=" + latittude + "&lon=" + longitude + "&APPID=038243b3b5b7501af0c45899e32c9e66";
+    var data;
+    var callback = function(response) {
+      console.log(response.weather);
+    };
 
+    $.getJSON(url, callback);
 
+  };
+  navigator.geolocation.getCurrentPosition(success);
 
-var success=function(position){
-           var coordinates=position.coords;
-           var longitude=coordinates.longitude;
-           var latittude=coordinates.latitude;
-        var url="http://api.openweathermap.org/data/2.5/forecast/weather?lat="+latittude+"&lon="+longitude+"&APPID=038243b3b5b7501af0c45899e32c9e66";
-            var data;
-            var callback=function(response){
-                console.log(response.weather);
-            };    
-
-          $.getJSON(url,callback);
-      
-    
-
-}
-      navigator.geolocation.getCurrentPosition(success)
-        
-
-
-
-
-})
+});
