f70eb2b54a43dfc5a0668e3651b11afaac84202f
CrownClown00 <rossca.thompson@gmail.com>
2016-06-12 22:27:03 -0400

    buttton
    button :(

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ $(document).ready(function() {
      
      $("#tempChange").click(function(){
               
-             $("#temperature").toggleClass(".temperature .Celtemperature");
+             $("#temperature").toggleClass("temperature Celtemperature");
 
      })
       //$("#temperature").append("<h1>Today's Weather: "+Fartemp+"</h1>");
